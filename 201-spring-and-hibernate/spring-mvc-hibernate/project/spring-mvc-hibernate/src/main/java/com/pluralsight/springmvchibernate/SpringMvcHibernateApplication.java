482779edcd86a48629ba970426c1385f9254d6f7
Balaji Sridharan <balaji.sridharan.ext@nokia.com>
2020-04-07 22:41:50 +0530

    Initial Commit for Spring MVC with Hibernate

---
+++ 201-spring-and-hibernate/spring-mvc-hibernate/project/spring-mvc-hibernate/src/main/java/com/pluralsight/springmvchibernate/SpringMvcHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.pluralsight.springmvchibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcHibernateApplication.class, args);
+	}
+
+}
---
+++ 201-spring-and-hibernate/spring-mvc-hibernate/project/spring-mvc-hibernate/src/test/java/com/pluralsight/springmvchibernate/SpringMvcHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluralsight.springmvchibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
